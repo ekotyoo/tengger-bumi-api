5b0056122797c04df9c518716941501e53ef793a
ekotyoo <ekeprasetyo@gmail.com>
2023-05-23 20:00:02 +0700

    setup procfile

--- src/validations/comment.validator.ts
+++ src/validations/comment.validator.ts
@@ -2,7 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import Joi from "joi";
 import validationHandler from "../middlewares/validator.middleware";
 
-
 export const commentValidator = (req: Request, res: Response, next: NextFunction) => {
     const schema = Joi.object({
         comment: Joi.string().required(),
